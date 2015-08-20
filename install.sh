845ea58aa686135d13af9afe14a92a35572b4a0d
v.promzelev <v.promzelev@fun-box.ru>
2015-08-20 16:26:13 +0300

    add ansible

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-cp vimrc ~/.vimrc
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +BundleInstall +quitall
+#!/bin/sh
+sudo easy_install pip
+sudo pip install paramiko PyYAML Jinja2 httplib2 six
+sudo pip install ansible
+
+#run ansible
+
+
+# cp vimrc ~/.vimrc
+# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# vim +BundleInstall +quitall
 #need git, ack, rubocop, ruby, etc(see vimrc)
