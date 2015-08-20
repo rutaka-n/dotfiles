17f3f6080ba50b3417876c578aff4d3fbe00d835
v.promzelev <v.promzelev@fun-box.ru>
2015-08-20 17:52:58 +0300

    update

--- install.sh
+++ install.sh
@@ -4,9 +4,4 @@ sudo pip install paramiko PyYAML Jinja2 httplib2 six
 sudo pip install ansible
 
 #run ansible
-
-
-# cp vimrc ~/.vimrc
-# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# vim +BundleInstall +quitall
-#need git, ack, rubocop, ruby, etc(see vimrc)
+ansible-playbook -i local work_environment.yml --ask-sudo-pass
